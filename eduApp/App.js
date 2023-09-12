2a70485de7e5bf4e528cba3f28df117e6a94bbd3
drake <vekiepd@gmail.com>
2023-09-13 00:13:11 +0700

    initialization setup

--- eduApp/App.js
+++ eduApp/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
@@ -9,7 +9,11 @@ export default function App() {
           source={{ uri: 'hotel2.jpg' }}
           style={{ width: 200, height: 200}}
         /> */}
-        <Image resizeMode="contain" source={require('./assets/images/START.png')} style={{ width: "100%", height: "100%" }} />
+      <Image
+        resizeMode="cover"
+        source={require('./assets/images/START.png')}
+        // style={{ width: '100%', height: '100%' }}
+      />
       <StatusBar style="auto" />
     </View>
   );
