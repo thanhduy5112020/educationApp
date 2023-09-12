58db64f30321b12e7c38ecedf1c2a75e993ae845
Duy Nguyen <thanhduy5112020@gmail.com>
2023-09-12 21:57:46 +0700

    first image

--- eduApp/App.js
+++ eduApp/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      {/* <Text>Hello World 1</Text> */}
+      {/* <Image
+          source={{ uri: 'hotel2.jpg' }}
+          style={{ width: 200, height: 200}}
+        /> */}
+        <Image resizeMode="contain" source={require('./assets/images/START.png')} style={{ width: "100%", height: "100%" }} />
       <StatusBar style="auto" />
     </View>
   );
