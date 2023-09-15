6bfffa49e1bc28391cc2d72646e374894479a8cc
Duy Nguyen <thanhduy5112020@gmail.com>
2023-09-15 22:08:46 +0700

    Advertising Screen demo

--- eduApp/src/screens/AdvertisingScreen.js
+++ eduApp/src/screens/AdvertisingScreen.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 import Slider from '../components/Carousel/Slider';
 
-const AdvertisingScreen = () => {
+const AdvertisingScreen = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text
+      {/* <Text
         style={{
           fontFamily: 'SUIT',
           fontWeight: 'bold',
           fontSize: 12,
-          // lineHeight: '47.7px',
         }}
       >
         청능 훈련을 통해 일상생활속의 대화를 연결시켜드려요!
-      </Text>
-      <Slider />
+      </Text> */}
+      <Image
+        resizeMode="cover"
+        source={require('../../assets/images/background.png')}
+      />
+
+      <TouchableOpacity
+        title="Navigate to next screen"
+        style={styles.customButton}
+        onPress={() => {
+          navigation.navigate('Start');
+        }}
+      >
+        <Image
+          resizeMode="cover"
+          source={require('../../assets/images/ButtonSlider.png')}
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -26,6 +41,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  customButton: {
+    position: 'absolute',
+    zIndex: 1,
+    top: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // backgroundColor: '#DDDDDD',
+    padding: 10,
+    borderRadius: 4,
   },
 });
 
